535eadb2afa5303ca1b625614059ff7407c6343a
sadiyaafrin25 <sadiyafrin21@gmail.com>
2025-08-28 12:41:24 +0600

    website frame done

---
+++ script.js
@@ -0,0 +1,39 @@
+// heart-count//
+
+const heartCountE=document.getElementById("heartCount");
+let heartCount=0;
+const heartButtons=document.querySelectorAll(".heartBtn");
+heartButtons.forEach(btn => {
+btn.addEventListener("click" , () =>{
+    heartCount++ ;
+    heartCountE.textContent = heartCount;
+})
+})
+
+// copy count and hotline copy//
+
+const copyCountE = document.getElementById("copyCount");
+let copyCount=0;
+const copyButtons = document.querySelectorAll(".copyBtn");
+copyButtons.forEach(btn =>{
+    btn.addEventListener("click" , ()=>{
+         copyCount ++;
+        copyCountE.textContent = copyCount ;
+       
+    })
+    })
+     const hotline = btn.parentElement.querySelector(".hotline").textContent.trim();
+    navigator.clipboard.writeText(hotline).then(() => {
+         alert(`copied : {hotline}`);
+})
+
+// coin call history//
+ let coins = 100;
+const coinDisplay = document.getElementById("coinDisplay");
+ const callHistoryList = document.getElementById("callHistory");
+ const clearHistoryBtn = document.getElementById("clearHistory");
+
+ function updateCoinDisplay() {
+      coinDisplay.textContent = coins + " Coins";
+    }
+ 
