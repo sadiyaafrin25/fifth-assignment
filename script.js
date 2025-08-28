52a20cf7962ee54dd0f085287f7ee32594b7e29a
sadiyaafrin25 <sadiyafrin21@gmail.com>
2025-08-28 18:33:15 +0600

    addeds codes for calling feature

--- script.js
+++ script.js
@@ -22,41 +22,51 @@ copyButtons.forEach(btn =>{
        
     })
     })
-     const hotline = btn.parentElement.querySelector(".hotline").textContent.trim();
-    navigator.clipboard.writeText(hotline).then(() => {
-         alert(`copied : {hotline}`);
-})
+    
 
-// coin call history//
- let coins = 100;
+
+
+//  clearHistoryBtn.addEventListener("click",()=>{
+// callHistoryList.innerHTML=" ";
+//  })
+//  updateCoinDisplay();
+
+let coins = 100; 
 const coinDisplay = document.getElementById("coinDisplay");
- const callHistoryList = document.getElementById("callHistory");
- const clearHistoryBtn = document.getElementById("clearHistory");
+const callHistoryList = document.getElementById("callHistory");
+const clearHistoryBtn = document.getElementById("clearhistory");
 
- function updateCoinDisplay() {
-      coinDisplay.textContent = coins + " Coins";
+
+function updateCoinDisplay() {
+  coinDisplay.textContent = coins + " Coins";
+}
+
+
+document.querySelectorAll(".callBtn").forEach(button => {
+  button.addEventListener("click", () => {
+    const card = button.parentNode; 
+
+    
+    const serviceName = card.children[0].innerText;
+    const serviceNumber = card.children[1].innerText;
+
+    if (coins < 20) {
+      alert(" Not enough coins! You need at least 20 coins to make a call.");
+      return;
     }
- document.querySelectorAll("callBtn").forEach(button => {
-    button.addEventListener("click", ()=>{
-        const card = button.parentElement;
-        const service = card.querySelector(".service").innerText;
-        const serviceNum = card.querySelector(".serviceNum").innerText;
+    coins -= 20;
+    updateCoinDisplay();
 
-        if(coins<20){
-            alert("Not Enough Coins !");
-            return;
-        }
-        coins-=20;
-        updateCoinDisplay();
-        alert(`calling ${service} (${serviceNum})`);
+    alert(` Calling ${serviceName} (${serviceNumber})`);
 
-        const li = document.createElement("li");
-        li.textContent=`${service} - ${serviceNum}`;
-        callHistoryList.appendChild(li);
-    })
- })
+    const li = document.createElement("li");
+    li.textContent = `${serviceName} - ${serviceNumber}`;
+    callHistoryList.appendChild(li);
+  });
+});
 
- clearHistoryBtn.addEventListener("click",()=>{
-callHistoryList.innerHTML=" ";
- })
- updateCoinDisplay();
+clearHistoryBtn.addEventListener("click", () => {
+  callHistoryList.innerHTML = "";
+});
+
+updateCoinDisplay();
